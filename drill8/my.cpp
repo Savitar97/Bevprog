f15d2bdd32eeba030ef22497ab39823ec8c5687e
Savitar97 <43468256+Savitar97@users.noreply.github.com>
2018-12-07 10:00:14 +0100

    Add files via upload

---
+++ drill8/my.cpp
@@ -0,0 +1,30 @@
+#include "std_lib_facilities.h"
+#include "my.h"
+
+int foo;
+
+void print_foo() 
+{
+    cout << foo;
+}
+
+void print(int i)
+{
+    cout << i; 
+}
+
+void swap_v(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap_r(int& a, int& b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
